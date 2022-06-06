d826413fc4db5139450872a49f87b25c031eb0a2
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-06-06 11:31:30 -0700

    Toastify alerts and logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,27 @@
+import { getAuth } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
 function Profile() {
-    return (
-      <h1>Profile</h1>
-    )
+  const auth = getAuth() 
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+  
+  return <div className='profile'>
+    <hedaer className="profileHeader">
+      <p className="pageHeader">My Profile</p>
+      <button type='button' className="logOut" onClick={onLogout}>Logout</button>
+    </hedaer>
+  </div>
   }
   
   export default Profile
