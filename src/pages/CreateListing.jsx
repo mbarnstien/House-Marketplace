1c5f786354be00547961630ce34afe0d96018a3f
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-06-10 14:35:18 -0700

    Upload images and save listings to firestore

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect, useRef } from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
+import {db} from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import {toast} from 'react-toastify'
+import {v4 as uuidv4} from 'uuid'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -100,7 +104,63 @@ function CreateListing() {
             geolocation.lng = longitude
             location = address
         }
+
+        //store image in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image)
+
+                uploadTask.on('state_changed', (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    console.log('Upload is ' + progress + '% done')
+                    switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused')
+                        break;
+                    case 'running':
+                        console.log('Upload is running')
+                        break;
+                    }
+                }, (error) => {
+                    reject(error)
+                }, () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                    })
+                }
+                )
+            })
+        }
+
+        const imageUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        const formDataCopy = {
+            ...formData,
+            imageUrls,
+            geolocation,
+            timestamp: serverTimestamp()
+        }
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+        location && (formDataCopy.location = location)
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
         setLoading(false)
+        toast.success('Listing saved')
+        navigate(`/categories/${formDataCopy.type}/${docRef.id}`)
     }
 
     const onMutate = (e) => {
