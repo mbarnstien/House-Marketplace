f707e7a86c0597b4d6f70b3e1e43e96aac739982
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-06-13 10:19:24 -0700

    Fetch single listing and listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -102,7 +102,6 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
-            location = address
         }
 
         //store image in firebase
@@ -152,9 +151,9 @@ function CreateListing() {
             timestamp: serverTimestamp()
         }
 
+        formDataCopy.location = address
         delete formDataCopy.images
         delete formDataCopy.address
-        location && (formDataCopy.location = location)
         !formDataCopy.offer && delete formDataCopy.discountedPrice
 
         const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
